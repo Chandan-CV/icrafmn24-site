9a8cdff95d39e33f0449d03c9a3c10065102aabd
kevinthomas <kevvin.thomas@gmail.com>
2024-01-13 11:56:53 +0530

    changes

--- src/components/MainHero.svelte
+++ src/components/MainHero.svelte
@@ -12,25 +12,25 @@
 <div class="pl-20 pt-20 overflow-hidden">
 	<div class="w-3/4">
 		<p
-			class="font-InterTight text-3xl md:text-6xl lg:text-6xl sm:text-6xl font-semibold text-[#09275E]"
+			class="font-InterTight text-3xl md:text-6xl lg:text-6xl sm:text-6xl font-semibold text-[#FFFFFF]"
 		>
 			ICRAFMN-24
 		</p>
-		<p class="font-InterTight text-xl md:text-2xl lg:text-2xl font-light text-[#5D5D5D]">
+		<p class="font-InterTight text-xl md:text-2xl lg:text-2xl font-semibold text-stone-300">
 			International Conference on Recent Advances in Fluid Mechanics and Nanoelectronics
 		</p>
-		<p class="font-InterTight text-lg md:text-xl lg:text-xl font-semibold text-[#042BF8]">
+		<p class="font-InterTight text-xl md:text-xl lg:text-xl font-normal text-[#84daff]">
 			Organised by Manipal Institute of Technology, Bangalore
 		</p>
 	</div>
-	<div class=" text-2xl font-InterTight font-normal pt-5 text-[#09275E] pb-4">July 12 - 14</div>
+	<div class=" text-2xl font-InterTight font-bold tracking-wider pt-5 text-[#ffffff] pb-4">July 12 - 14</div>
 	<div class="flex flex-row ">
 		<div class="flex flex-col justify-center md:mt-[-200px]">
 			<p class=" text-2xl md:text-3xl lg:text-3xl font-semibold text-[#09275E] pb-3 flex flex-col">
 				About
 			</p>
 			<div class="flex overflow-x-hidden max-w-full overflow-hidden ">
-				<p class="text-lg md:text-xl lg:text-xl text-[#505050] text-balance">
+				<p class="text-lg md:text-xl lg:text-xl text-[#ffffff] text-balance">
 					A three-day International Conference on Recent Advances in Fluid Mechanics and Nanoelectronics (ICRAFMN – 2023)is being hosted by Manipal Institute of Technology (MIT)
 					Bengaluru in association with NIT Uttarakhand from 12th-14th July 2023 at MIT Bengaluru.
 					The conference aims to provide an excellent venue for multidisciplinary academicians,
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -5,7 +5,7 @@
 	import Navbar from "../components/Navbar.svelte";
   </script>
   
-  <body class="from-[#ffffff] to-[#9ec0ff] bg-gradient-to-b overflow-x-hidden h-screen">
+  <body class="from-[#9382ff] to-[#2974ff] bg-gradient-to-b overflow-x-hidden h-screen">
     <Header/>
     <Navbar/>
     <slot />

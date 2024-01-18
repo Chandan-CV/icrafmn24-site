2557993830a91166b8e26c781ba47f365c9c6cab
Chandan <chandancvp08@gmail.com>
2024-01-18 12:18:50 +0530

    added the publications section

--- src/components/Highlights.svelte
+++ src/components/Highlights.svelte
@@ -26,7 +26,7 @@
 	</div>
 	<div class="w-full flex flex-col justify-center items-center py-10">
 		<div class="text-2xl md:text-3xl lg:text-3xl font-semibold text-[#09275E] pb-5 flex flex-col text-center mt-10 mb-5">Previous Publications</div>
-		<div class="flex gap-5 flex-wrap  text-center">
+		<div class="flex gap-5 flex-wrap  text-center justify-center">
 			<a href="https://pubs.aip.org/pof/collection/15930/Recent-Advances-in-Fluid-Mechanics-and" target="_blank" class="bg-blue-950 p-5 text-white rounded-full shadow shadow-blue-600 font-semibold hover:bg-white hover:text-blue-600 transition-colors ease-in-out">Physics of Fluids</a>
 			<a href="https://pubs.aip.org/jap/collection/15930/Recent-Advances-in-Fluid-Mechanics-and" target="_blank" class="bg-blue-950 p-5 text-white rounded-full shadow shadow-blue-600 font-semibold hover:bg-white hover:text-blue-600 transition-colors ease-in-out">Journal of Applied Physics</a>
 		</div>

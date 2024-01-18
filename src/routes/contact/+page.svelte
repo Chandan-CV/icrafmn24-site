e65e524d05d7c7393eb38c7cc600169591f04ad0
Chandan <chandancvp08@gmail.com>
2024-01-18 10:25:43 +0530

    contact up page

--- src/routes/contact/+page.svelte
+++ src/routes/contact/+page.svelte
@@ -1,14 +1,17 @@
 <div class="flex items-center flex-col px-8">
     <h1 class="font-InterTight text-3xl font-semibold text-[#09275E]">Contact</h1>
-    <div class="grid grid-cols-2 justify-center md:grid-cols-3 lg:grid-cols-4 text-center items-center gap-5 py-10">
-        {#each contacts as {name, department, college, title, email, phone}}
-            <div class="bg-blue-200 px-5 py-5 rounded-xl items-center flex flex-col shadow-2xl">
+    <div class="flex flex-row flex-wrap justify-center text-center items-center gap-5 py-10">
+        {#each contacts as {name, designation, organization, role, email, phone}}
+            <div class="bg-white px-5 py-5 rounded-xl items-center flex flex-col shadow-2xl">
                 <h1 class="text-black font-InterTight text-xl lg:text-2xl font-bold">{name}</h1>
-                <h1 class="text-stone-700 lg:text-lg font-InterTight">{department}</h1>
-                <h1 class="text-stone-700 lg:text-lg font-InterTight">{college}</h1>
-                <h1 class="text-stone-700 lg:text-xl font-InterTight font-semibold text-lg py-2">{title}</h1>
+                <h1 class="text-stone-700 lg:text-lg font-InterTight">{designation}</h1>
+                <h1 class="text-stone-700 lg:text-lg font-InterTight">{organization}</h1>
+                <h1 class="text-stone-700 lg:text-xl font-InterTight font-semibold text-lg py-2">{role}</h1>
                 <a href={`mailto:${email}`}><h1 class="text-blue-600 font-InterTight lg:text-lg">{email}</h1></a>
+                {#if phone}
                 <a href={`tel:${phone}`}><h1 class="text-stone-700 underline file:font-InterTight lg:text-lg">{phone}</h1></a>
+                    
+                {/if}
 
             </div>
         {/each}
@@ -16,63 +19,5 @@
 </div>
 
 <script>
-    const contacts = [
-        {
-            name : "Dr. Rajshekhar Chaudhary",
-            department : "Dept. of Mathematics",
-            college : "MIT Bengaluru",
-            title : "Convener, ICRAFMN-24",
-            email : "icrafmn2024@manipal.edu",
-            phone : "+918123158384"
-        },
-        {
-            name : "Dr. Rajshekhar Chaudhary",
-            department : "Dept. of Mathematics",
-            college : "MIT Bengaluru",
-            title : "Convener, ICRAFMN-24",
-            email : "icrafmn2024@manipal.edu",
-            phone : "+918123158384"
-        },
-        {
-            name : "Dr. Rajshekhar Chaudhary",
-            department : "Dept. of Mathematics",
-            college : "MIT Bengaluru",
-            title : "Convener, ICRAFMN-24",
-            email : "icrafmn2024@manipal.edu",
-            phone : "+918123158384"
-        },
-        {
-            name : "Dr. Rajshekhar Chaudhary",
-            department : "Dept. of Mathematics",
-            college : "MIT Bengaluru",
-            title : "Convener, ICRAFMN-24",
-            email : "icrafmn2024@manipal.edu",
-            phone : "+918123158384"
-        },
-        {
-            name : "Dr. Rajshekhar Chaudhary",
-            department : "Dept. of Mathematics",
-            college : "MIT Bengaluru",
-            title : "Convener, ICRAFMN-24",
-            email : "icrafmn2024@manipal.edu",
-            phone : "+918123158384"
-        },
-        {
-            name : "Dr. Rajshekhar Chaudhary",
-            department : "Dept. of Mathematics",
-            college : "MIT Bengaluru",
-            title : "Convener, ICRAFMN-24",
-            email : "icrafmn2024@manipal.edu",
-            phone : "+918123158384"
-        },
-        {
-            name : "Dr. Rajshekhar Chaudhary",
-            department : "Dept. of Mathematics",
-            college : "MIT Bengaluru",
-            title : "Convener, ICRAFMN-24",
-            email : "icrafmn2024@manipal.edu",
-            phone : "+918123158384"
-        },
-
-    ]
+    import contacts from "./contactData";
 </script>
---
+++ src/routes/contact/contactData.ts
@@ -0,0 +1,60 @@
+interface ContactPerson {
+    name: string;
+    designation?: string;
+    organization?: string;
+    role?: string;
+    email: string;
+    phone?: string;
+  }
+  
+  const contacts: ContactPerson[] = [
+    {
+      name: "Dr. Kalpna Sharma",
+      designation: "HOD, Department of Mathematics",
+      organization: "MUJ Jaipur",
+      role: "Convener, ICRAFMN-2024",
+      email: "kalpana.sharma@jaipur.manipal.edu",
+      phone: "+(91) 8890036666"
+    },
+    {
+      name: "Dr. Rajashekhar V. Choudhari",
+      designation: "Dept. of Mathematics",
+      organization: "MIT, Bengaluru",
+      role: "Convener, ICRAFMN-2023",
+      email: "rv.choudhari@manipal.edu",
+      phone: "+(91) 8123158384"
+    },
+    {
+      name: "Dr. Dharmendra Tripathi",
+      designation: "Dept. of Mathematics",
+      organization: "NIT Uttarakhand",
+      role: "Chairperson, ICRAFMN-2023",
+      email: "dtripathi@nituk.ac.in",
+      phone: "+(91) 8385882005"
+    },
+    {
+      name: "Dr. Alok Bhargava",
+      designation: "Department of Mathematics and Statistics",
+      organization: "MUJ Jaipur",
+      role: "Co-Convenor, ICRAFMN-2024",
+      email: "alok.bhargava@jaipur.manipal.edu",
+      phone: "+(91) 9414073300"
+    },
+    {
+      name: "Dr. Jagriti Sahariya",
+      designation: "Department of Physics",
+      organization: "NIT, Uttarakhand",
+      role: "Co-Convenor, ICRAFMN-2024",
+      email: "jagrati@nituk.ac.in"
+    },
+    {
+      name: "Dr. Manjunatha M S",
+      designation: "Department of Mechanical and Industrial Engineering",
+      organization: "MIT, Bengaluru",
+      role: "Co-Convenor, ICRAFMN-2024",
+      email: "manjunath.ms@manipal.edu",
+      phone: "+(91) 9483426391"
+    }
+  ];
+  
+  export default contacts;

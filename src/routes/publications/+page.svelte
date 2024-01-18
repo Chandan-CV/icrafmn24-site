c71362095620a867ed55cff7eeaa0b9e3b39846e
Chandan <chandancvp08@gmail.com>
2024-01-18 10:34:27 +0530

    added publications page

---
+++ src/routes/publications/+page.svelte
@@ -0,0 +1,10 @@
+
+
+<div class="">
+    <div class="min-h-screen flex flex-col items-center pt-10 px-10">
+        <h1 class="font-InterTight text-3xl font-semibold text-[#09275E] pb-3">Publications</h1>
+        <div class="flex flex-col md:flex-row lg:flex-row  w-full text-4xl font-bold text-center items-center justify-center mt-20">
+      TO BE UPDATED SOON
+        </div>
+    </div>
+</div>

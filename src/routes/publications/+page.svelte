ee698f314609d28fe11c9ed9c75d2688cefb2deb
Chandan <chandancvp08@gmail.com>
2024-01-24 07:32:35 +0530

    publications page

--- src/routes/publications/+page.svelte
+++ src/routes/publications/+page.svelte
@@ -1,10 +1,22 @@
-
+<!-- Publication
+The selected papers will be published free of cost (no APC) in the special issue of Critical Reviews in Biomedical Engineering (ISSN: 0278-940X, Scopus Indexed, Quartile-3) after peer review process.
+Note: Some more Scopus and Web of Science indexed publications will be added after the final consent from the publishers related to fluid mechanics and nanoelectronics. (On bottom with blinking) -->
 
 <div class="">
-    <div class="min-h-screen flex flex-col items-center pt-10 px-10">
-        <h1 class="font-InterTight text-3xl font-semibold text-orange-950 pb-3">Publications</h1>
-        <div class="flex flex-col md:flex-row lg:flex-row  w-full text-4xl font-bold text-center items-center justify-center mt-20">
-      TO BE UPDATED SOON
-        </div>
-    </div>
+	<div class="min-h-screen flex flex-col items-center pt-10 px-10">
+		<h1 class="font-InterTight text-3xl font-semibold text-orange-950 pb-10">Publications</h1>
+		<div>
+			<p class="font-InterTight text-2xl font-light text-black pb-3">
+				The selected papers will be published free of cost (no APC) in the special issue of Critical
+				Reviews in Biomedical Engineering (ISSN: 0278-940X, Scopus Indexed, Quartile-3) after peer
+				review process.
+			</p>
+		</div>
+		<div class="mt-16">
+			<p class="font-InterTight text-2xl font-semibold text-black pb-3 text-center animate-pulse">
+			 <strong>	Note: </strong> Some more Scopus and Web of Science indexed publications will be added after the final
+				consent from the publishers related to fluid mechanics and nanoelectronics.
+			</p>
+		</div>
+	</div>
 </div>

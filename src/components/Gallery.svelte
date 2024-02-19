0e80eb5eea345a57b132a54ba56b7142d5b22261
Chandan <chandancvp08@gmail.com>
2024-02-19 16:12:06 +0530

    removed dumb imports

--- src/components/Gallery.svelte
+++ src/components/Gallery.svelte
@@ -1,14 +1,7 @@
 <script>
 	import { Splide, SplideSlide } from '@splidejs/svelte-splide';
-    import I6 from '../lib/images/WhatsApp Image 2024-01-13 at 12.31.30.jpeg';
-    import I2 from '../lib/images/WhatsApp Image 2024-01-13 at 12.33.01.jpeg';
-    import I3 from '../lib/images/NewImage.jpeg';
-    import I4 from '../lib/images/WhatsApp Image 2024-01-13 at 12.33.58.jpeg';
-    import I5 from '../lib/images/WhatsApp Image 2024-01-13 at 12.36.20.jpeg';
-    import I1 from '../lib/images/WhatsApp Image 2024-01-13 at 12.39.13.jpeg';
     import '@splidejs/svelte-splide/css/skyblue';
     
-    const Images = [I1,I2,I3,I4,I5,I6];
     const newImages =[
         "https://firebasestorage.googleapis.com/v0/b/covid-19-tracker-9cad8.appspot.com/o/WhatsApp%20Image%202024-01-13%20at%2012.39.13.jpeg?alt=media&token=5faaf261-9411-4e47-aadd-a167d76d3b81",
         "https://firebasestorage.googleapis.com/v0/b/covid-19-tracker-9cad8.appspot.com/o/WhatsApp%20Image%202024-01-13%20at%2012.33.01.jpeg?alt=media&token=73bc5abf-d916-4085-804d-529e72ee7036",

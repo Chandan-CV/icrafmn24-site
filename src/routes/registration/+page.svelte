f0c6244cb2fed60a20aa77c5f06451439e35a7dd
Chandan <chandancvp08@gmail.com>
2024-02-25 17:01:09 +0530

    added the register now button

--- src/routes/registration/+page.svelte
+++ src/routes/registration/+page.svelte
@@ -1,59 +1,97 @@
-<div class="flex items-center flex-col px-8">
-    <h1 class="font-InterTight text-3xl font-semibold text-orange-950">Online Registration and Payment</h1>
-    <div class="w-full py-10">
-    <h1 class="text-black font-InterTight font-semibold text-2xl pb-5">Fee Table</h1>
-    <table class="table-auto text-black font-InterTight w-full text-center text-lg">
-        <tr class="">
-          <th colspan="5" class="font-semibold">Registration Fee (last date April 15, 2024)</th>
-        </tr>
-        <tr class="text-sm font-bold">
-          <td>Nationality</td>
-          <td>Research Scholars</td>
-          <td>UG/PG Students</td>
-          <td>Faculty</td>
-          <td>R&D Organizations/
+<script>
+</script>
 
-            Industry</td>
-        </tr>
-        <tr>
-          <td>Indian</td>
-          <td>INR 4000/-*</td>
-          <td>INR 3000/-*</td>
-          <td>INR 6000/-*</td>
-          <td>INR 8000/-*</td>
-        </tr>
-        <tr>
-          <td>International</td>
-          <td colspan="4">USD 100/-*</td>
-        </tr>
-      </table>
-      <div class="py-5 flex flex-col gap-3 text-justify">
-        <p>*Including GST, registration kit, certificate, breakfast, lunch, tea/coffee during the conference.</p>
-        <p>*Special Discount in the registration fees will be offered to IAESTE membership students and participants belonging to SAARC countries. To avail the special discount please write an email to icrafmn2024@manipal.edu.</p>
-        <p>The conference registration fee includes abstract presentation and food during the conference. There are no article processing charges for publishing papers in special issues.</p>
-            <p class="font-semibold">
-                Accommodation will be provided in MUJ hostels (against payment) based on the availability. Further, there is also a provision for accompanying person and for more details regarding accommodation please feel free to contact Dr. Mohd. Rizwanullah | Dr. Virendra Singh Chouhan | Dr. Sandeep Kumar.
-            </p>
-            <p class="font-semibold text-red-600 bg-red-200 p-3 rounded-xl">
-                The registration is complete only after the successful payment of the registration fees. For participants from abroad, an invoice will be generated in your name and will be sent to you along with MAHE bank account details to complete the registration process. Please write to us at alok.bhargava@jaipur.manipal.edu for the invoice with your details.
-            </p>
-        <p class="text-lg font-bold">Only international participants can choose to present in a hybrid mode. But we'd like to meet people from other countries in person.</p>
-        <!-- <p class="text-lg font-bold">Students/Research Scholars without any financial support can request through email for free accommodation.</p> -->
-      </div>
-    </div>
-    <div class=" bg-orange-900 text-white p-5 rounded-2xl px-10 text-2xl font-semibold cursor-pointer mb-32 "> <a href="https://www.payumoney.com/customer/users/paymentOptions/#/1011F8DA7C0FCB5EC870FBB52B99DD08/ICRAFMN_2024/219353">Register Now</a></div>
+<div class="flex items-center flex-col px-8">
+	<h1 class="font-InterTight text-3xl font-semibold text-orange-950">
+		Online Registration and Payment
+	</h1>
+	<div class="w-full py-10">
+		<h1 class="text-black font-InterTight font-semibold text-2xl pb-5">Fee Table</h1>
+		<table class="table-auto text-black font-InterTight w-full text-center text-lg">
+			<tr class="">
+				<th colspan="5" class="font-semibold">Registration Fee (last date April 15, 2024)</th>
+			</tr>
+			<tr class="text-sm font-bold">
+				<td>Nationality</td>
+				<td>Research Scholars</td>
+				<td>UG/PG Students</td>
+				<td>Faculty</td>
+				<td>R&D Organizations/ Industry</td>
+			</tr>
+			<tr>
+				<td>Indian</td>
+				<td>INR 4000/-*</td>
+				<td>INR 3000/-*</td>
+				<td>INR 6000/-*</td>
+				<td>INR 8000/-*</td>
+			</tr>
+			<tr>
+				<td>International</td>
+				<td colspan="4">USD 100/-*</td>
+			</tr>
+		</table>
+		<div class="py-5 flex flex-col gap-3 text-justify">
+			<p>
+				*Including GST, registration kit, certificate, breakfast, lunch, tea/coffee during the
+				conference.
+			</p>
+			<p>
+				*Special Discount in the registration fees will be offered to IAESTE membership students and
+				participants belonging to SAARC countries. To avail the special discount please write an
+				email to icrafmn2024@manipal.edu.
+			</p>
+			<p>
+				The conference registration fee includes abstract presentation and food during the
+				conference. There are no article processing charges for publishing papers in special issues.
+			</p>
+			<p class="font-semibold">
+				Accommodation will be provided in MUJ hostels (against payment) based on the availability.
+				Further, there is also a provision for accompanying person and for more details regarding
+				accommodation please feel free to contact Dr. Mohd. Rizwanullah | Dr. Virendra Singh Chouhan
+				| Dr. Sandeep Kumar.
+			</p>
+			<p class="font-semibold text-red-600 bg-red-200 p-3 rounded-xl">
+				The registration is complete only after the successful payment of the registration fees. For
+				participants from abroad, an invoice will be generated in your name and will be sent to you
+				along with MAHE bank account details to complete the registration process. Please write to
+				us at alok.bhargava@jaipur.manipal.edu for the invoice with your details.
+			</p>
+			<p class="text-lg font-bold">
+				Only international participants can choose to present in a hybrid mode. But we'd like to
+				meet people from other countries in person.
+			</p>
+			<!-- <p class="text-lg font-bold">Students/Research Scholars without any financial support can request through email for free accommodation.</p> -->
+		</div>
+	</div>
+	<div class="flex w-full justify-evenly flex-row-reverse flex-wrap">
+		<div
+			class=" bg-orange-900 text-white p-5 rounded-2xl px-10 text-2xl font-semibold cursor-pointer mb-32"
+		>
+			<a
+				href="https://www.payumoney.com/customer/users/paymentOptions/#/1011F8DA7C0FCB5EC870FBB52B99DD08/ICRAFMN_2024/219353"
+				>ICRAFMN-2024 Payment Link</a
+			>
+		</div>
+
+		<div
+			class=" bg-orange-900 text-white p-5 rounded-2xl px-10 text-2xl font-semibold cursor-pointer mb-32"
+		>
+			<a
+				href="https://forms.gle/rJL88B6nMRp85VxH6"
+				>Register now</a
+			>
+		</div>
+	</div>
 </div>
 
 <style>
-    table, th, td {
-        border: 1px solid;
-    }
-    li {
-        display: list-item;
-        list-style-position: inside;
-    }
+	table,
+	th,
+	td {
+		border: 1px solid;
+	}
+	li {
+		display: list-item;
+		list-style-position: inside;
+	}
 </style>
-
-<script>
-
-</script>

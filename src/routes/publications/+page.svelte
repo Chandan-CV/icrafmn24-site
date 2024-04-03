8b9d7860596d358b9653d7102806d317461d5ff7
Chandan <chandancvp08@gmail.com>
2024-04-03 13:49:27 +0530

    changed the publications page

--- src/routes/publications/+page.svelte
+++ src/routes/publications/+page.svelte
@@ -5,7 +5,7 @@ Note: Some more Scopus and Web of Science indexed publications will be added aft
 <div class="">
 	<div class="min-h-screen flex flex-col items-center pt-10 px-10">
 		<h1 class="font-InterTight text-3xl font-semibold text-orange-950 pb-10">Publications</h1>
-		<div>
+		<!-- <div>
 			<p class="font-InterTight text-2xl font-light text-black pb-3">
 				The selected papers will be published with no APC charges in the special issue of Critical
 				Reviews in Biomedical Engineering (ISSN: 0278-940X, Scopus Indexed, Quartile-3).
@@ -13,20 +13,89 @@ Note: Some more Scopus and Web of Science indexed publications will be added aft
 		</div>
 		<div>
 			<p class="font-InterTight text-2xl font-light text-black pb-3">
-				• <strong>Journal</strong>: Critical Reviews in Biomedical Engineering, (ISSN: 0278-940X, Scopus Indexed,
-				Begell House, Quartile-3), <strong> Call for Papers: Special Issue: Mathematical Biology</strong> <br/> <br><br> • <strong> Journal</strong>:
-				Interactions, (ISSN: 1572-9540, Scopus Indexed, Springer Nature, Quartile-3),
-				<strong> Call for
-				Papers: Special Issue: ICRAFMN-2024: 2nd International Conference on Recent Advances in
-				Fluid Mechanics and Nanoelectronics</strong> <br><br><br> • <strong>Journal</strong>: Special Topics & Reviews in Porous Media: An
-				International Journal (ISSN: 2151-562X, Scopus and Web of Science (SCIE) Indexed, Begell
-				House, Quartile-2), <strong>Call for Papers: Special Issue: ICRAFMN-2024: Recent Mathematical Models
-				on Nanofluids Flow and Heat Transfer in Porous Media </strong>
-				<br><br><br>
+				• <strong>Journal</strong>: Critical Reviews in Biomedical Engineering, (ISSN: 0278-940X,
+				Scopus Indexed, Begell House, Quartile-3),
+				<strong> Call for Papers: Special Issue: Mathematical Biology</strong> <br /> <br /><br /> •
+				<strong> Journal</strong>: Interactions, (ISSN: 1572-9540, Scopus Indexed, Springer Nature,
+				Quartile-3),
+				<strong>
+					Call for Papers: Special Issue: ICRAFMN-2024: 2nd International Conference on Recent
+					Advances in Fluid Mechanics and Nanoelectronics</strong
+				> <br /><br /><br /> • <strong>Journal</strong>: Special Topics & Reviews in Porous Media:
+				An International Journal (ISSN: 2151-562X, Scopus and Web of Science (SCIE) Indexed, Begell
+				House, Quartile-2),
+				<strong
+					>Call for Papers: Special Issue: ICRAFMN-2024: Recent Mathematical Models on Nanofluids
+					Flow and Heat Transfer in Porous Media
+				</strong>
+				<br /><br /><br />
 			</p>
-		</div>
-	
-		
+		</div> -->
+
+		<p class ="text-2xl mb-10 font-bold">The selected papers will be published with no APC charges in the special issues of:</p>
+
+		<table class="border border-black text-center text-lg ">
+			<tr>
+				<td class="border border-black font-bold"> Journal: International Journal of Thermofluids </td>
+				<td class="border border-black font-bold">
+					Journal: Special Topics & Reviews in Porous Media: An International Journal
+				</td>
+				<td class="border border-black font-bold"> Journal: Interactions </td>
+			</tr>
+			<tr>
+				<td class="border border-black">
+					(ISSN: 2666-2027, Elsevier, Scopus Indexed, Quartile-1)
+				</td>
+				<td class="border border-black">
+					(ISSN: 2151-562X, Scopus and WoS (SCIE) Indexed, Begell House, Quartile-2)
+				</td>
+				<td class="border border-black">
+					(ISSN: 1572-9540, Scopus Indexed, Springer Nature, Quartile-3)
+				</td>
+			</tr>
+
+			<td class="border border-black">
+				Call for Papers-Special Issue: ICRAFMN-2024: Recent Advancements in Nanofluids and Hybrid
+				Nanofluids: Modelling, Simulation & Experimentation
+			</td>
+			<td class="border border-black">
+				Call for Papers-Special Issue: ICRAFMN-2024: Recent Mathematical Models on Nanofluids Flow
+				and Heat Transfer in Porous Media
+			</td>
+			<td>
+				Call for Papers-Special Issue: ICRAFMN-2024: 2nd International Conference on Recent Advances
+				in Fluid Mechanics and Nanoelectronics
+			</td>
+		</table>
+
+		<table class="border border-black text-center text-lg mt-20 ">
+			<tr>
+				<td class="border border-black font-bold"> Journal: Critical Reviews in Biomedical Engineering </td>
+				<td class="border border-black font-bold">
+					Journal: Science Talks
+				</td>
+			</tr>
+			<tr>
+				<td class="border border-black">
+					(ISSN: 0278-940X, Scopus Indexed,
+Begell House, Quartile-3),
+				</td>
+				<td class="border border-black">
+					(ISSN: 2772-5693, Elsevier)
+				</td>
+				
+			</tr>
+
+			<td class="border border-black">
+				Call for Papers-Special Issue:
+Mathematical Biology
+			</td>
+			<td class="border border-black">
+				Call for Papers-Special Issue:
+ICRAFMN-2024: Recent Advancements in Fluid and Heat Flow
+			</td>
+			
+		</table>
 		<div class="mt-16">
 			<p class="font-InterTight text-2xl font-semibold text-black pb-3 text-center animate-pulse">
 				<strong> Note: </strong> Some more Scopus and Web of Science indexed publications will be added
